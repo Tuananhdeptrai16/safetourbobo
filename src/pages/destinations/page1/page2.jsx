eb7f81909386dac9b44a09d55c298e1f6de637dd
Tuananhdeptrai16 <Tuananhpro1608@gmail.com>
2024-08-27 21:19:14 +0700

    fixed destination

--- src/pages/destinations/page1/page2.jsx
+++ src/pages/destinations/page1/page2.jsx
@@ -31,7 +31,7 @@ export const Page2 = () => {
       </div>
       <div className="container">
         <div className="destination__content">
-          <div className="row row-cols-4 gy-4 row-cols-lg-3 gy-lg-2 row-cols-md-1 gy-md-3">
+          <div className="row row-cols-4 gy-4 row-cols-lg-2 gy-lg-2 row-cols-md-1 gy-md-3">
             {destinations.map((item) => {
               return (
                 <div className="col" key={item.id}>
