c6b623b5ae697e95802dbcb5df3d46e0069b7f03
Tuananhdeptrai16 <Tuananhpro1608@gmail.com>
2024-08-27 21:59:31 +0700

    page number control

--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -1,15 +1,26 @@
-import React from "react";
 import { Pagination } from "antd";
-import { useNavigate } from "react-router-dom";
-
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 const PageNumber = () => {
+  const [current, setCurrent] = useState(1);
   const navigate = useNavigate();
-
+  const location = useLocation();
+  useEffect(() => {
+    // Lấy số trang từ URL bằng biểu thức chính quy
+    const match = location.pathname.match(/page\/(\d+)/);
+    console.log(match);
+    if (match) {
+      const page = parseInt(match[1], 10);
+      if (page !== current) {
+        setCurrent(page);
+      }
+    }
+  }, [location, current]);
   const onChangePage = (page) => {
     navigate(`/safetourbobo/destinations/page/${page}`);
   };
 
-  return <Pagination defaultCurrent={1} total={50} onChange={onChangePage} />;
+  return <Pagination current={current} total={50} onChange={onChangePage} />;
 };
 
 export default PageNumber;
